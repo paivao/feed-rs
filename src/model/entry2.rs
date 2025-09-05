84c49fa0402e9204adf233b93646734680c0ee1f
Rafael Paiva <08.paiva@gmail.com>
2025-09-04 22:54:38 -0400

    Using macro seems to be the best anti-boilerplate technique

--- src/model/entry2.rs
+++ src/model/entry2.rs
@@ -15,53 +15,61 @@ const TABLE_NAMES: &'static [&'static str] = &[
 ];
 
 
-#[derive(Default)]
-struct BaseEntry {
-    id: i64,
-    enabled: bool,
-    description: String,
-    valid_until: Option<DateTime<Utc>>,
-    //feed_id: i64,
+macro_rules! make_entry_type {
+    ($name: ident, $field_type: ty, $table_name: literal) => {
+        #[derive(Debug, Clone, FromRow)]
+        pub struct $name {
+            pub id: i64,
+            pub value: $field_type,
+            pub enabled: bool,
+            pub description: String,
+            pub valid_until: Option<DateTime<Utc>>,
+            //feed_id: i64,
+        }
+
+        impl $name {
+            pub async fn insert(conn: &PgPool, feed: &Feed, value: $field_type, description: Option<String>, valid_until: Option<DateTime<Utc>>) -> Result<Self, Error> {
+                let descr = description.unwrap_or(String::new());
+                let id:i64 = sqlx::query_scalar(concat!("INSERT INTO ", $table_name, " (value, enabled, feed_id, description, valid_until) VALUES $1, $2, $3, $4, $5 RETURNING id;"))
+                .bind(&value)
+                .bind(true)
+                .bind(feed.id)
+                .bind(&descr)
+                .bind(&valid_until)
+                .fetch_one(conn).await?;
+                Ok(Self{id: id, value: value, enabled: true, description: descr, valid_until: valid_until/*, feed_id: feed.id*/})
+            }
+        }
+    }
 }
 
-#[derive(FromRow)]
-struct IPEntry {
-    inner: BaseEntry,
-    value: IpNetwork,
-}
+make_entry_type!(IPEntry, IpNetwork, "ip_entries");
 
+/* 
 pub trait TEntry where
     for <'r> Self: Sized + AsRef<BaseEntry> + AsMut<BaseEntry> + Unpin + FromRow<'r, PgRow> + Send,
     for <'q> Self::ValueType: Encode<'q, Postgres> + Type<Postgres> + Sync + Unpin + Decode<'q, Postgres> + Send  
 {
     type ValueType;
-    const INSERT_QUERY: &'static str = "INSERT INTO {} (value, enabled, feed_id, description, valid_until) VALUES $1, $2, $3, $4, $5 RETURNING id;";
+    const INSERT_QUERY: &'static str = ;
     const FETCH_QUERY: &'static str = "SELECT value from {} WHERE feed_id = $1 AND enabled = TRUE AND (valid_until IS NULL OR valid_until >= NOW())";
-    const GET_SOME_QUERY: &'static str = "SELECT (id, value, enabled, description, valid_until) from {} WHERE feed_id = $1";
+    const GET_SOME_QUERY: &'static str = "SELECT (id, value, enabled, description, valid_until) from {} WHERE feed_id = ";
 
     fn mut_value(&mut self) -> &mut Self::ValueType;
     fn value(&self) -> &Self::ValueType;
     fn new(value: Self::ValueType, inner: BaseEntry) -> Self;
 
-    async fn insert(conn: &PgPool, feed: &Feed, value: Self::ValueType, description: Option<String>, valid_until: Option<DateTime<Utc>>) -> Result<Self, Error> {
-        let descr = description.unwrap_or(String::new());
-        let id:i64 = sqlx::query_scalar(Self::INSERT_QUERY)
-            .bind(&value)
-            .bind(true)
-            .bind(feed.id)
-            .bind(&descr)
-            .bind(&valid_until)
-            .fetch_one(conn).await?;
-        Ok(Self::new(value, BaseEntry{id: id, enabled: true, description: descr, valid_until: valid_until/*, feed_id: feed.id*/}))
-    }
+    
 
     fn fetch_values<'q>(conn: &'q PgPool, feed: &Feed) -> BoxStream<'q, Result<Self::ValueType, Error>> where Self::ValueType: 'q
     {
+        sqlx::quer
         sqlx::query_scalar(Self::FETCH_QUERY).bind(feed.id).fetch(conn)
     }
 
-    async fn fetch_some(conn: &PgPool, feed: &Feed, window: (i64, i64), enabled: Option<bool>, valid_until: Option<Option<DateTime<Utc>>>) -> Result<Vec<Self>, Error> {
+    async fn fetch_some(conn: &PgPool, feed: &Feed, quantity: i64, last_id: Option<i64>, enabled: Option<bool>, valid_until: Option<Option<DateTime<Utc>>>) -> Result<Vec<Self>, Error> {
         let mut builder = sqlx::QueryBuilder::new(Self::GET_SOME_QUERY);
+        builder.push_bind(feed.id);
         if let Some(enabled_cond) = enabled {
             builder.push(" AND enabled = ").push_bind(enabled_cond);
         }
@@ -72,7 +80,41 @@ pub trait TEntry where
                 None => builder.push(" IS NULL")
             };
         }
+        builder.push(" AND id > ").push_bind(last_id.unwrap_or(0));
+        builder.push(" LIMIT ").push_bind(quantity);
         builder.build_query_as().fetch_all(conn).await
+    }
+
+    async fn update(&self, conn: &PgPool) -> Result<(), Error> {
+        sqlx::query("UPDATE")
     }
 }
 
+impl AsRef<BaseEntry> for IPEntry {
+    fn as_ref(&self) -> &BaseEntry {
+        &self.inner
+    }
+}
+
+impl AsMut<BaseEntry> for IPEntry  {
+    fn as_mut(&mut self) -> &mut BaseEntry {
+        &mut self.inner
+    }
+}
+
+impl TEntry for IPEntry {
+    type ValueType = IpNetwork;
+    
+    fn mut_value(&mut self) -> &mut Self::ValueType {
+        &mut self.value
+    }
+    
+    fn value(&self) -> &Self::ValueType {
+        &self.value
+    }
+    
+    fn new(value: Self::ValueType, inner: BaseEntry) -> Self {
+        Self{value: value, inner: inner}
+    }
+}
+    */
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,11 +1,3 @@
-mod entry;
-mod entry2;
-mod feed;
-
-pub struct Window(i64, i64);
-
-impl Window {
-    pub fn new(last_inserted: i64, size: i64) -> Window {
-        Window(last_inserted, size)
-    }
-}
+//mod entry;
+pub mod entry2;
+pub mod feed;
