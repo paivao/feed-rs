d22df7a0887277a125d65c0c7c646fa60b9420af
Rafael Paiva <08.paiva@gmail.com>
2025-09-14 10:56:31 -0400

    Created an simple, yet versatille logging infrascructure

---
+++ src/log.rs
@@ -0,0 +1,55 @@
+use std::{env, fs::OpenOptions, str::FromStr as _};
+
+use reopen::Reopen;
+use simplelog::{ConfigBuilder, LevelFilter, SharedLogger, TermLogger, WriteLogger};
+
+const LOG_SINKS: [&str; 2] = ["app", "access"];
+
+pub fn logging_bootstrap(app_name: &str) {
+    let mut sinks = Vec::<Box<dyn SharedLogger>>::with_capacity(LOG_SINKS.len());
+    
+    let config = ConfigBuilder::new()
+        .set_target_level(LevelFilter::Error)
+        .set_time_format_rfc3339()
+        .set_level_color(simplelog::Level::Error, Some(simplelog::Color::Red))
+        .set_level_color(simplelog::Level::Info, Some(simplelog::Color::Green)).to_owned();
+    
+    for sink in LOG_SINKS {
+        let env_var_name = format!("{}_LEVEL", sink.to_uppercase());
+        let level_env_var_name = format!("{}_LEVEL", &env_var_name);
+        let log_level = env::var(&level_env_var_name).unwrap_or(String::from("info"));
+        let log_level = LevelFilter::from_str(&log_level).expect(&format!("Invalid log level for {}: {}", &level_env_var_name, log_level));
+        let config = config.clone().add_filter_allow(format!("{}::{}", app_name, sink)).build();
+        let env_var = env::var(&env_var_name).unwrap_or(String::from("stderr"));
+        sinks.push(match env_var.as_ref() {
+            "stderr" => TermLogger::new(log_level, config, simplelog::TerminalMode::Stderr, simplelog::ColorChoice::Auto),
+            file => {
+                let file_name = String::from(file);
+                let fd = Reopen::new(Box::new(move ||open_log_file(file_name.to_owned()))).expect(&format!("Unable to open log file for {}: {}", &env_var_name, file));
+                // If system is unix, 
+                #[cfg(unix)]
+                actix_web::rt::spawn(async || {
+                    let mut sig = signal(SignalKind::hangup())?;
+                    loop {
+                        sig.recv().await;
+                        fd.handle().reopen();
+                    }
+                });
+                WriteLogger::new(log_level, config, fd)
+            }
+        });
+    };
+
+}
+
+fn create_syslog_logger() {
+
+}
+
+fn open_log_file(log_file: String) -> std::io::Result<std::fs::File> {
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(log_file)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+use simplelog::SharedLogger;
 use sqlx::postgres::{PgConnectOptions, PgPool, PgPoolOptions};
 use std::env;
+use std::fs::{File, OpenOptions};
 use dotenvy::dotenv;
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use actix_web::web::{self, Redirect};
 use actix_web::middleware::Logger;
 use actix_files as fs;
 
+use crate::log::logging_bootstrap;
+
 mod model;
 mod controller;
+mod log;
 
 const APP_NAME: &str = "feed-rs";
 
 const DEFAULT_MAX_DB_CONNS: u32 = 5;
-const DEFAULT_DB: &str = "feedme";
-const DEFAULT_PORT: u16 = 8080;
+//const DEFAULT_DB: &str = "feedme";
+const DEFAULT_BIND_PORT: u16 = 8080;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -25,11 +30,16 @@ async fn main() -> std::io::Result<()> {
     // First, get configuration
     dotenv().expect(r#"Configuration ".env" file not found."#);
     // Second, connect to database
+    let conn_options = PgConnectOptions::new()
+        .application_name(APP_NAME);
     let pool = PgPoolOptions::new()
         .max_connections(get_env_with_default("DB_POOL_MAX_CONNS", DEFAULT_MAX_DB_CONNS))
-        .connect_with(PgConnectOptions::new().application_name(APP_NAME)).await.expect("Unable to connect to database");
+        .connect_with(conn_options).await.expect("Unable to connect to database");
     // And to migrate structure
     sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
+    // Setting logging sinks
+    logging_bootstrap(APP_NAME);
 
     // And then, web serve
     HttpServer::new(move ||{
@@ -42,14 +52,17 @@ async fn main() -> std::io::Result<()> {
             .service(fs::Files::new("/admin", "./public"))
             // Feed list service
             .service(controller::feed::serve_feed)
-
+            // API
+            .service(web::scope("/api")
+                .configure(controller::feed::configure_feed_api)
+            )
             .service(hello)
     }).bind_auto_h2c(get_bind_addr())?.run().await
 }
 
 fn get_bind_addr() -> (String, u16) {
     let host = env::var("BIND_HOST").unwrap_or(String::from("127.0.0.1"));
-    let port = get_env_with_default("BIND_PORT", DEFAULT_PORT);
+    let port = get_env_with_default("BIND_PORT", DEFAULT_BIND_PORT);
     (host, port)
 }
 
@@ -57,3 +70,4 @@ fn get_bind_addr() -> (String, u16) {
 fn get_env_with_default<T>(var: &str, default: T) -> T where T: std::str::FromStr + Copy {
     env::var(var).map_or(default, |x| x.parse().unwrap_or(default))
 }
+
